76a75dd33cbed0ec59b6761a0e75af7b30bdc279
Shafa Auliya <92239898+ShafaAuliya21@users.noreply.github.com>
2021-11-27 10:23:43 +0700

    Add files via upload

---
+++ Basic Data Types.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string a;
+    string b;
+    string c;
+    string d;
+    string e;
+    
+    cin >> a >> b >> c >> d >> e;
+    
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << d << endl;
+    cout << e << endl;
+    
+    return 0;
+}
